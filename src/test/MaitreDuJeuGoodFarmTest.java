1da08180fb17150dbec124f66c75506e3422d379
p.akpata <pierreakpata8@gmail.com>
2022-03-14 10:49:41 +0100

    Ajout de la la bibliothèque AreneGoodFarm.jar

--- src/test/MaitreDuJeuGoodFarmTest.java
+++ src/test/MaitreDuJeuGoodFarmTest.java
@@ -1,7 +1,5 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.awt.Point;
